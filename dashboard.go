bd5a044bc8c47c427432bc28def9272a319f5e7b
Simon Whitear <swhitear@groupon.com>
2015-10-15 12:14:55 -0500

    Applied patch for reconecting to redis on each refresh

--- dashboard.go
+++ dashboard.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
+        "html/template"
+        "net/http"
 )
 
 func DashboardHandler(configuration *Configuration, response http.ResponseWriter, request *http.Request) (int, error) {
         response.Header().Set("Content-type", "text/html")
 
+        connection := connect(configuration.Redis.Host, configuration.Redis.Port)
+
         err := request.ParseForm()
         if err != nil {
-		http.Redirect(response, request, "/error", 307)
+                http.Redirect(response, request, "/error", 307)
         }
 
-        blacklisted  := replyToArray(configuration.Redis.Connection.Cmd("KEYS", "*:repsheet:ip:blacklisted"))
-	whitelisted  := replyToArray(configuration.Redis.Connection.Cmd("KEYS", "*:repsheet:ip:whitelisted"))
-	marked       := replyToArray(configuration.Redis.Connection.Cmd("KEYS", "*:repsheet:ip:marked"))
+        blacklisted  := replyToArray(connection.Cmd("KEYS", "*:repsheet:ip:blacklisted"))
+        whitelisted  := replyToArray(connection.Cmd("KEYS", "*:repsheet:ip:whitelisted"))
+        marked       := replyToArray(connection.Cmd("KEYS", "*:repsheet:ip:marked"))
         templates, _ := template.ParseFiles("templates/layout.html", "templates/dashboard.html")
         summary      := Summary{Blacklisted: blacklisted, Whitelisted: whitelisted, Marked: marked}
         templates.ExecuteTemplate(response, "layout", Page{Summary: summary, Active: "dashboard"})
 
-	return 200, nil
+        return 200, nil
 }
